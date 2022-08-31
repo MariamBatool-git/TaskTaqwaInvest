1c98bba3d773849016e86c2b967a1396cfb3f970
MariamBatool <mariammalik.mb462@gmail.com>
2022-08-31 14:26:33 +0500

    added the signup endpoint successfully

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
 
+
+
+dbConnect();
 // body parser configuration
 app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +15,37 @@ app.use(bodyParser.json());
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
+});
+
+// saving data to mongodb
+app.post("/signup", (request, response) => {
+  bcrypt.hash(request.body.password, 10)
+  .then((hashedPassword) => {
+    const user = new User({
+      email: request.body.email,
+      password: hashedPassword,
+    });
+    user.save()
+    .then((result) => {
+      response.status(201).send({
+        message: "User Created Successfully",
+        result,
+      });
+    })
+    // catch error if the new user wasn't added successfully to the database
+    .catch((error) => {
+      response.status(500).send({
+        message: "Error creating user",
+        error,
+      });
+    });
+})
+  .catch((e) => {
+    response.status(500).send({
+      message: "Password was not hashed successfully",
+      e,
+    });
+  })
 });
 
 
--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,25 +1,36 @@
-const {MongoClient} = require('mongodb');
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
+// external imports
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+async function listDatabases(){
+    //databasesList = await mongoose.db().admin().listDatabases();
  
     console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    //databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
- 
-async function main() {
-    const uri = "mongodb+srv://MariamBatool:mary5376854@cluster0.3mc9aw2.mongodb.net/authDB?retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
-    
-        await listDatabases(client);
-     
-    } catch (e) {
-        console.error(e);
-    }
-    finally {
-        await client.close();
-    }      
+
+async function dbConnect() {
+    const DB_URL = "mongodb+srv://MariamBatool:mary5376854@cluster0.3mc9aw2.mongodb.net/authDB?retryWrites=true&w=majority";
+  // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
+  await mongoose
+    .connect(
+        process.env.DB_URL || DB_URL,
+      {
+        //   these are options to ensure that the connection is done properly
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+    await listDatabases();
 }
-main().catch(console.error);
+
+module.exports = dbConnect;
+
